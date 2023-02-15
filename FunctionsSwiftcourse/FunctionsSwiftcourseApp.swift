96588be2f29a8290a2e2755a55e633ece3773022
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-02-15 16:31:39 +0000

    Initial Commit

---
+++ FunctionsSwiftcourse/FunctionsSwiftcourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionsSwiftcourseApp.swift
+//  FunctionsSwiftcourse
+//
+//  Created by Tony Chaidinis on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FunctionsSwiftcourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
